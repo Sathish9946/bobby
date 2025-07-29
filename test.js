d729502d08f6c4fa41153345775b961a5405bfcb
Sathish9946 <i.sathish2893@gmail.com>
2025-07-29 12:52:00 +0530

    first

---
+++ test.js
@@ -0,0 +1,9 @@
+let a=2
+let b=5
+//console.log(a+b);
+
+let x=3
+let y=5
+let z=9
+console.log(y+z);
+
---
+++ test1.js
@@ -0,0 +1,5 @@
+let myname = 'Bobby'
+let age = 25
+let roll = 'Tester'
+let salary = 55999
+console.log(myname+' ' + age+' ' + roll+' ' + salary);
